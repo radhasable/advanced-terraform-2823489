c686fdb33b26485e09b78341fd36149eb9116425
Your Name <you@example.com>
2021-04-10 17:53:36 +0530

    changes_made_to_variables_part1

--- 02_01_variables/practice/mainbak.tf
+++ 02_01_variables/practice/mainbak.tf
@@ -1,8 +1,4 @@
-# //////////////////////////////
-# VARIABLES
-# //////////////////////////////
-variable "aws_access_key" {}
-///here curly empty braces have no default value hence the default type is string 
+variable "aws_access_key" {}  //here the value will be derived from the tfvars file
 variable "aws_secret_key" {}
 
 variable "region" {
@@ -10,24 +6,23 @@ variable "region" {
 }
 
 variable "vpc_cidr" {
-  default = "172.16.0.0/16"
+  default = "172.16.0.0/16"  
 }
 
-variable "subnet1_cidr" {
+variable "subnet_cidr" {
   default = "172.16.0.0/24"
 }
 
 variable "environment_list" {
   type = list(string)
-  default = ["DEV","QA","STAGE","PROD"]
+  default = ["DEV", "STG", "PROD"]
 }
 
 variable "environment_map" {
   type = map(string)
   default = {
     "DEV" = "DEV",
-    "QA" = "QA",
-    "STAGE" = "STAGE",
+    "STG" = "STG",
     "PROD" = "PROD"
   }
 }
@@ -36,8 +31,7 @@ variable "environment_instance_type" {
   type = map(string)
   default = {
     "DEV" = "t2.micro",
-    "QA" = "t2.micro",
-    "STAGE" = "t2.micro",
+    "STG" = "t2.micro",
     "PROD" = "t2.micro"
   }
 }
@@ -46,78 +40,72 @@ variable "environment_instance_settings" {
   type = map(object({instance_type=string, monitoring=bool}))
   default = {
     "DEV" = {
-      instance_type = "t2.micro", 
-      monitoring = false
+    instance_type = "t2.micro",
+    monitoring = false
     },
-   "QA" = {
-      instance_type = "t2.micro", 
-      monitoring = false
-    },
-    "STAGE" = {
-      instance_type = "t2.micro", 
-      monitoring = false
+    "STG" = {
+    instance_type = "t2.micro",
+    monitoring = false
     },
     "PROD" = {
-      instance_type = "t2.micro", 
-      monitoring = true
+    instance_type = "t2.micro",
+    monitoring = false
     }
-  }
+   }
 }
 
 variable "deploy_environment" {
   default = "DEV"
 }
-# //////////////////////////////
-# PROVIDERS
-# //////////////////////////////
+
+
+//////////PROVIDER/////////////
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   region     = var.region
 }
 
-# //////////////////////////////
-# RESOURCES
-# //////////////////////////////
+///////RESOURCES//////////
 
-# VPC
+//VPC
 resource "aws_vpc" "vpc1" {
   cidr_block = var.vpc_cidr
   enable_dns_hostnames = "true"
 }
 
-# SUBNET
+//SUBNET
 resource "aws_subnet" "subnet1" {
   cidr_block = var.subnet1_cidr
-  vpc_id = aws_vpc.vpc1.id
+  vpc_id = aws_vpc.vpc1.vpc_id
   map_public_ip_on_launch = "true"
   availability_zone = data.aws_availability_zones.available.names[1]
 }
 
-# INTERNET_GATEWAY
-resource "aws_internet_gateway" "gateway1" {
-  vpc_id = aws_vpc.vpc1.id
+//INTERNET GATEWAY
+resource "aws_internet_gateway" "igw1" {
+  vpc_id = aws_vpc.vpc1.vpc_id
 }
 
-# ROUTE_TABLE
+//ROUTE TABLE
 resource "aws_route_table" "route_table1" {
-  vpc_id = aws_vpc.vpc1.id
-
+  vpc_id = aws_vpc.vpc1.vpc_id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gateway1.id
+    gateway_id = aws_internet_gateway.igw1.gateway_id
   }
 }
 
-resource "aws_route_table_association" "route-subnet1" {
-  subnet_id = aws_subnet.subnet1.id
-  route_table_id = aws_route_table.route_table1.id
+//ROUTE TABLE ASSOCIATION
+resource "aws_route_table_association" "route_subnet1" {
+  subnet_id = aws_subnet.subnet1.subnet_id
+  route_table_id = aws_route_table.route_table1.route_table_id
 }
 
-# SECURITY_GROUP
+//SECURITY GROUP
 resource "aws_security_group" "sg-nodejs-instance" {
-  name = "nodejs_sg"
-  vpc_id = aws_vpc.vpc1.id
+  name = "nodejs-sg"
+  vpc_id  =aws_vpc.vpc1.vpc_id
 
   ingress {
     from_port = 80
@@ -132,7 +120,7 @@ resource "aws_security_group" "sg-nodejs-instance" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   egress {
     from_port = 0
     to_port = 0
@@ -141,52 +129,50 @@ resource "aws_security_group" "sg-nodejs-instance" {
   }
 }
 
-# INSTANCE
+//INSTANCE
+
 resource "aws_instance" "nodejs1" {
   ami = data.aws_ami.aws-linux.id
-  //instance_type = var.environment_instance_type["DEV"]
-  instance_type = var.environment_instance_settings[var.deploy_environment].instance_type
-  subnet_id = aws_subnet.subnet1.id
-  vpc_security_group_ids = [aws_security_group.sg-nodejs-instance.id]
+  instance_type = var.environment_instance_type[var.deploy_environment]
+ // instance_type = var.environment_instance_settings["DEV"].instance_type
 
+  subnet_id = aws_subnet.subnet1.subnet_id
+  vpc_security_group_ids = [aws_security_group.nodejs_sg.id]
   monitoring = var.environment_instance_settings[var.deploy_environment].monitoring
-
-  tags = {Environment = var.environment_map[var.deploy_environment]}
-
+  tags = {Environment = var.environment_list[0]}
 }
 
-# //////////////////////////////
-# DATA
-# //////////////////////////////
+
+//DATA
+
 data "aws_availability_zones" "available" {}
 
 data "aws_ami" "aws-linux" {
   most_recent = true
-  owners      = ["amazon"]
+  owners = ["amazon"]
 
   filter {
-    name   = "name"
+    name = "name"
     values = ["amzn-ami-hvm*"]
   }
 
   filter {
-    name   = "root-device-type"
+    name = "root-device-type"
     values = ["ebs"]
-  }
+    }
 
   filter {
-    name   = "virtualization-type"
+    name = "virtualization-type"
     values = ["hvm"]
   }
 }
 
-# //////////////////////////////
-# OUTPUT
-# //////////////////////////////
-output "instance-dns" {
+
+//OUTPUT
+output "instance_dns" {
   value = aws_instance.nodejs1.public_dns
 }
 
-output "private-dns" {
+output "private_dns" {
   value = aws_instance.nodejs1.private_dns
 }
